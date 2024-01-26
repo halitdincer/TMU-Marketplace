24978c0404478181164da691efab087b85cf5114
Halit Dincer <github@halitdincer.com>
2024-01-26 05:18:37 -0500

    added filter option for list view of ad

--- src/ads/views.py
+++ src/ads/views.py
@@ -3,8 +3,24 @@ from .models import Ad
 from .serializers import AdSerializer
 
 class AdListView(ListAPIView):
-    queryset = Ad.objects.all()
     serializer_class = AdSerializer
+
+    def get_queryset(self):
+        queryset = Ad.objects.all()
+        category = self.request.query_params.get('category')
+        min_price = self.request.query_params.get('min_price', None)
+        max_price = self.request.query_params.get('max_price', None)
+
+        if category is not None:
+            queryset = queryset.filter(category=category)
+
+        if min_price is not None:
+            queryset = queryset.filter(price__gte=min_price)
+
+        if max_price is not None:
+            queryset = queryset.filter(price__lte=max_price)
+
+        return queryset
 
 class AdDetailView(RetrieveAPIView):
     queryset = Ad.objects.all()
