5095f51617787327651ce5ebaa300915324358db
Halit Dincer <github@halitdincer.com>
2024-02-07 15:03:53 -0500

    serve react build folder from django server

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,14 +119,22 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# The path of react build folder
+REACT_APP_BUILD_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', 'client', 'build'))
+REACT_APP_STATIC_PATH = os.path.abspath(os.path.join(BASE_DIR, '..', 'client', 'build', 'static'))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    REACT_APP_STATIC_PATH,
+]
--- src/public/urls.py
+++ src/public/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from django.conf import settings
+
+from .views import serve_react
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path(r"^(?P<path>.*)$", serve_react, {"document_root": settings.REACT_APP_BUILD_PATH}),
 ]
