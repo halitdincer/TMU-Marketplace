90469a0168512013257791ecf78ff2e44f8e91ee
Halit Dincer <github@halitdincer.com>
2024-03-06 15:00:23 -0500

    created chat applicatin in backend for handling chat functionality

--- client/src/pages/InboxPage.jsx
+++ client/src/pages/InboxPage.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import AdsList from 'components/AdsList'
 import Header from 'components/Header';
 import Sidebar from 'components/Sidebar';
+import { socket } from 'utils/socket';
 
 function InboxPage({isConnected}){
+
+  function connect() {
+    alert("Starting the connection");
+    socket.connect();
+  }
+
+  function disconnect() {
+    alert("Ending the connection");
+    socket.disconnect();
+  }
+
   return (
     <div className="flex">
       <Sidebar />
@@ -13,6 +25,8 @@ function InboxPage({isConnected}){
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <h1 className="text-5xl font-semibold">Inbox</h1>
             <p>State: { '' + isConnected }</p>
+            <button onClick={ connect }>Connect</button>
+            <button onClick={ disconnect }>Disconnect</button>
           </div>
         </div>
       </div>
