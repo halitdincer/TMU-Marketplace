e3ee56d9a8b20fb68c89aa5a0367a6919a6d4075
Halit Dincer <github@halitdincer.com>
2024-03-11 16:15:22 -0400

    create the message model, also serializer and views for it to return all messages with filters

--- server/core/urls.py
+++ server/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/ads/', include('ads.urls')),
+    path('api/messages/', include('chat.urls')),
     path('admin/', admin.site.urls),
     path('', include('public.urls')),
 ]
