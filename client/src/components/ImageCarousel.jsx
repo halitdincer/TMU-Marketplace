438a01ea17231596eb19bf650de9275ce540d556
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-03-25 18:43:04 -0400

    Made image height responsive for Mobile

--- client/src/components/ImageCarousel.jsx
+++ client/src/components/ImageCarousel.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
+import React, { useState } from "react";
+import { useWindowSize } from "@react-hook/window-size";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 function ImageCarousel({ images }) {
-
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [width, height] = useWindowSize();
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -23,25 +24,25 @@ function ImageCarousel({ images }) {
   };
 
   return (
-    <div className='max-w-[1400px] h-[500px] w-full m-auto px-4 relative group'>
+    <div className="max-w-[1400px] h-[300px] lg:h-[500px] w-full m-auto px-4 relative group">
       <div
         style={{ backgroundImage: `url(${images[currentIndex].image_url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-100'
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-100"
       ></div>
       {/* Left Arrow */}
-      <div className=' group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <div className=" group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactLeft onClick={prevSlide} size={20} />
       </div>
       {/* Right Arrow */}
-      <div className=' group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <div className=" group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactRight onClick={nextSlide} size={20} />
       </div>
-      <div className='flex top-4 justify-center py-2'>
+      <div className="flex top-4 justify-center py-2">
         {images.map((slide, slideIndex) => (
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
+            className="text-2xl cursor-pointer"
           >
             <RxDotFilled />
           </div>
