3fc21ddc874d8d8a7d35e2668730298dd2bc94b5
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-03-25 15:56:49 -0400

    Fixing the routing issue

--- client/src/components/AdsList.jsx
+++ client/src/components/AdsList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AdCard from './AdCard';
+import { Link } from 'react-router-dom';
 
 function AdsList() {
   // State to store the ads
@@ -43,7 +44,10 @@ function AdsList() {
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-2 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           {ads.length > 0 ? (
             ads.map(ad => (
+              <Link to={`/ad/${ad.id}`}>
               <AdCard key={ad.id} ad={ad} />
+              </Link>
+              
             ))
           ) : (
             <p>No ads found.</p>
