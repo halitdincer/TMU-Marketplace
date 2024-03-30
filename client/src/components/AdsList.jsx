0bf341e36780ed7fea34d64f859254acf8ce3fc5
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-03-30 19:30:27 -0400

    Fixed routing for adCard

--- client/src/components/AdsList.jsx
+++ client/src/components/AdsList.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import AdCard from './AdCard';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useMemo } from "react";
+import AdCard from "./AdCard";
+import { Link } from "react-router-dom";
 
 function AdsList({ searchQuery }) {
   // State to store the ads
@@ -12,17 +12,27 @@ function AdsList({ searchQuery }) {
   // State to store any potential error from the fetch operation
   const [error, setError] = useState(null);
 
-  const filteredAds = useMemo(() => ads.filter(ad => {
-    const lowerSearchQuery = searchQuery ? searchQuery.toLowerCase() : '';
-    return (ad.title ? ad.title.toLowerCase().includes(lowerSearchQuery) : false) || 
-           (ad.description ? ad.description.toLowerCase().includes(lowerSearchQuery) : false);
-  }), [ads, searchQuery]);
+  const filteredAds = useMemo(
+    () =>
+      ads.filter((ad) => {
+        const lowerSearchQuery = searchQuery ? searchQuery.toLowerCase() : "";
+        return (
+          (ad.title
+            ? ad.title.toLowerCase().includes(lowerSearchQuery)
+            : false) ||
+          (ad.description
+            ? ad.description.toLowerCase().includes(lowerSearchQuery)
+            : false)
+        );
+      }),
+    [ads, searchQuery]
+  );
 
   useEffect(() => {
     // Function to fetch ads data
     const fetchAds = async () => {
       try {
-        const response = await fetch('/api/ads/');
+        const response = await fetch("/api/ads/");
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -44,23 +54,21 @@ function AdsList({ searchQuery }) {
 
   return (
     <>
-    
-    <div className="bg-white py-6 sm:py-6">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-2 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-4">
-          {ads.length > 0 ? (
-            filteredAds.map(ad => (
-              //<Link to={`/ad/${ad.id}`}>
-              <AdCard key={ad.id} ad={ad} />
-              //</Link>
-            ))
-          ) : (
-            <p>No ads found.</p>
-          )}
+      <div className="bg-white py-6 sm:py-6">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-2 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+            {ads.length > 0 ? (
+              filteredAds.map((ad) => (
+                <Link to={`/ad/${ad.id}`}>
+                  <AdCard key={ad.id} ad={ad} />
+                </Link>
+              ))
+            ) : (
+              <p>No ads found.</p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-
     </>
   );
 }
