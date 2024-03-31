1547d9de8cbc0237b4463b546c48615b0eef31ba
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-03-31 18:58:30 -0400

    conditonal rendering for mobile responsiveness

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function Avatar({ avatarUrl, altText }) {
   return (
@@ -7,43 +7,116 @@ function Avatar({ avatarUrl, altText }) {
     </div>
   );
 }
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const hours = date.getHours().toString().padStart(2, "0"); // Get hours and pad with 0 if single digit
+  const minutes = date.getMinutes().toString().padStart(2, "0"); // Get minutes and pad with 0 if single digit
+  return `${hours}:${minutes}`;
+}
 
 function ChatMessage({ message, isSender }) {
-  const messageClass = isSender ? "flex justify-end mb-4 cursor-pointer" : "flex mb-4 cursor-pointer";
-  const messageBoxClass = isSender ? "flex max-w-96 bg-indigo-500 text-white rounded-lg p-3 gap-3" : "flex max-w-96 bg-white rounded-lg p-3 gap-3";
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    // Initial check on mount
+    handleResize();
+    // Listen to window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const messageClass = isSender
+    ? "flex justify-end  mb-4 cursor-pointer lg:pr-0 pr-2"
+    : "flex mb-4 cursor-pointer  lg:pl-0 pl-2";
+  const messageBoxClass = isSender
+    ? "flex lg:max-w-96 max-w-64 bg-indigo-500 text-white rounded-tl-xl rounded-tr-xl rounded-bl-xl p-3 gap-3 "
+    : "flex lg:max-w-96 max-w-64 bg-gray-100 rounded-tl-xl rounded-tr-xl rounded-br-xl p-3 gap-3 ";
   const avatarUrl = isSender
     ? "https://placehold.co/200x/b7a8ff/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
     : "https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato";
-  
+
   return (
     <div className={messageClass}>
-      {!isSender && <Avatar avatarUrl={avatarUrl} altText="User Avatar" />}
-      <div className={messageBoxClass}>
-        <p>{message.text}</p>
-      </div>
-      {isSender && <Avatar avatarUrl={avatarUrl} altText="My Avatar" />}
+      {!isSender && (
+        <>
+          {!isMobile ? (
+            <Avatar avatarUrl={avatarUrl} altText="User Avatar" />
+          ) : null}
+          <div className={messageBoxClass}>
+            <p>{message.text}</p>
+          </div>
+          <p className="text-xs ml-1 text-gray-500 self-end">
+            {formatTimestamp(message.timestamp)}
+          </p>
+        </>
+      )}
+
+      {isSender && (
+        <>
+          <p className="text-xs ml-1 text-gray-500 self-end pr-1">
+            {formatTimestamp(message.timestamp)}
+          </p>
+          <div className={messageBoxClass}>
+            <p>{message.text}</p>
+          </div>
+
+          {!isMobile ? (
+            <Avatar avatarUrl={avatarUrl} altText="My Avatar" />
+          ) : null}
+        </>
+      )}
     </div>
   );
 }
 
 function ChatWindow({ messages, userId }) {
   // Sort messages by timestamp before rendering
-  const sortedMessages = messages.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-  
+  const sortedMessages = messages.sort(
+    (a, b) => new Date(a.timestamp) - new Date(b.timestamp)
+  );
+
   return (
-    <div className="flex-1 relative">
-      <header className="bg-white p-4 text-gray-700">
-        <h1 className="text-2xl font-semibold">{messages[0].receiver_name}</h1>
+    <div className="flex-1  border-l border-gray-300 pt-0">
+      <header className="bg-white lg:p-4 pl-10 pb-3 text-gray-700 border-b border-gray-100 flex items-center">
+        <div className="w-10 h-10 rounded-full flex items-center justify-center mr-2">
+          <img
+            src={
+              "https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
+            }
+            alt={"myavatar"}
+            className="w-10 h-10 rounded-full"
+          />
+        </div>
+        <h1 className="text-2xl font-semibold">{messages[0].sender_name}</h1>
       </header>
-      <div className="h-screen overflow-y-auto p-4 pb-36">
+
+      <div className="h-screen overflow-y-auto lg:p-4 pr-2 p-2 pb-36">
         {sortedMessages.map((msg) => (
-          <ChatMessage key={msg.id} message={msg} isSender={msg.sender === userId} />
+          <ChatMessage
+            key={msg.id}
+            message={msg}
+            isSender={msg.sender === userId}
+          />
         ))}
       </div>
       <footer className="bg-white p-4 absolute bottom-0 w-full">
         <div className="flex items-center">
-          <input type="text" placeholder="Type a message..." className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500" />
-          <button className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">Send</button>
+          <input
+            type="text"
+            placeholder="Type a message..."
+            className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500"
+          />
+          <button className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">
+            Send
+          </button>
         </div>
       </footer>
     </div>
