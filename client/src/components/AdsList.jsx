0625df5cd76bb8dcc5c018dbb82169e686e368f4
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-03-31 00:15:05 -0400

    Added bottom padding for mobile responsiveness

--- client/src/components/AdsList.jsx
+++ client/src/components/AdsList.jsx
@@ -54,7 +54,7 @@ function AdsList({ searchQuery }) {
 
   return (
     <>
-      <div className="bg-white py-6 sm:py-6">
+      <div className="bg-white py-6 sm:py-6 pb-24">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-2 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-4">
             {ads.length > 0 ? (
