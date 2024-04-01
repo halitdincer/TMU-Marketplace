e46c40859ad87496ae76d9eac2e8e7b2297e4609
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-04-01 18:24:34 -0400

    Fixed positioning for sidebar

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -6,9 +6,8 @@ import CreateAdForm from "components/CreateAdForm";
 function CreatePage({ onAdSubmit }) {
   return (
     <div className="flex ">
-      <div className="fixed left-0 top-0 h-full">
-        <Sidebar />
-      </div>
+      <Sidebar />
+
       <div className="flex-1 overflow-y-auto lg:ml-20 ml-0">
         <CreateAdForm />
       </div>
