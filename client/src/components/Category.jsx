0986cb196c077e91eae1556cbe01dce96cecb732
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-04-03 21:15:59 -0400

    added a "<" button that directs to categories

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,30 +1,29 @@
-import React, { useState, useEffect, useContext } from 'react';
-import AdCard from './AdCard';
-import { Link, useLocation } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import { useMemo } from 'react';
+import React, { useState, useEffect, useContext } from "react";
+import AdCard from "./AdCard";
+import { Link, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useMemo } from "react";
 
 import Header from "components/Header";
 import Sidebar from "components/Sidebar";
 import HeaderLoggedIn from "components/HeaderLoggedIn";
 import { AuthContext } from "components/AuthProvider";
 
-
 const categories = {
-  'EL': 'Electronics',
-  'CL': 'Clothing',
-  'SP': 'Sports & Outdoors',
-  'GH': 'Games & Hobbies',
-  'MU': 'Music & Instruments',
-  'FA': 'Furniture & Appliances',
-  'BE': 'Beauty & Personal Care',
-  'GA': 'Garden',
-  'TB': 'Textbooks',
-  'LO': 'Lost & Found',
-  'SG': 'Study Groups',
-  'TU': 'Tutoring',
-  'RS': 'Research & Surveys',
-  'OT': 'Others',
+  EL: "Electronics",
+  CL: "Clothing",
+  SP: "Sports & Outdoors",
+  GH: "Games & Hobbies",
+  MU: "Music & Instruments",
+  FA: "Furniture & Appliances",
+  BE: "Beauty & Personal Care",
+  GA: "Garden",
+  TB: "Textbooks",
+  LO: "Lost & Found",
+  SG: "Study Groups",
+  TU: "Tutoring",
+  RS: "Research & Surveys",
+  OT: "Others",
 };
 
 const Category = () => {
@@ -32,18 +31,14 @@ const Category = () => {
   const { userData } = useContext(AuthContext);
   const { category } = useParams(); // This extracts the category from the URL
 
-
   const [ads, setAds] = useState([]);
-
 
   // State to store loading status
   const [isLoading, setIsLoading] = useState(true);
 
-
   // State to store any potential error from the fetch operation
   const [error, setError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
-
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     const fetchAdsByCategory = async () => {
@@ -62,7 +57,6 @@ const Category = () => {
         setIsLoading(false); // Set loading to false since the data is loaded or failed
       }
     };
-
 
     fetchAdsByCategory(); // Call the fetch function
   }, [category]); // Dependency array with category to refetch when category changes
@@ -83,7 +77,6 @@ const Category = () => {
     [ads, searchQuery]
   );
 
-
   const categoryName = categories[category] || "Category";
 
   const handleSearchSubmit = (query) => {
@@ -93,18 +86,39 @@ const Category = () => {
     <div className="flex">
       <Sidebar />
       <div className="flex-1 sm:ml-28">
-
         {/* Display the appropriate header based on user authentication status */}
         {userData ? (
           <HeaderLoggedIn onSearchSubmit={handleSearchSubmit} />
         ) : (
           <Header onSearchSubmit={handleSearchSubmit} />
         )}
-        <div className="bg-custom-blue pl-6 p-2">
+        <div className="bg-custom-blue pl-3 p-2 flex items-center">
+          <Link to="/categories" className="text-white mr-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </Link>
           <h1 className="text-xl text-white font-bold">{categoryName}</h1>
         </div>
         {/* Conditional rendering based on loading, error, and ads state */}
-        <div className={`mx-auto mt-10 pt-2 ${isLoading || error || ads.length === 0 ? 'p-12' : "grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 p-8 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-4"}`}>
+        <div
+          className={`mx-auto mt-10 pt-2 ${
+            isLoading || error || ads.length === 0
+              ? "p-12"
+              : "grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 p-8 sm:mt-5 sm:pt-5 lg:mx-0 lg:max-w-none lg:grid-cols-4"
+          }`}
+        >
           {isLoading ? (
             <div>Loading...</div>
           ) : error ? (
@@ -122,7 +136,6 @@ const Category = () => {
       </div>
     </div>
   );
-
-}
+};
 
 export default Category;
